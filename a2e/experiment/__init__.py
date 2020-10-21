da951a7d3329da0ed91eec0da70df734929233c0
Markus Mächler <markus.maechler@bithost.ch>
2020-10-21 14:10:32 +0200

    Adds history and plots to experiment log

--- a2e/experiment/__init__.py
+++ a2e/experiment/__init__.py
@@ -4,5 +4,4 @@ The :mod:`a2e.experiment` provides utility functions to create and log experimen
 
 from ._git import git_hash
 from ._git import git_diff
-from ._experiment import create_experiment
 from ._experiment import Experiment
--- a2e/experiment/_experiment.py
+++ a2e/experiment/_experiment.py
@@ -2,23 +2,34 @@ import inspect
 import json
 import os
 import pathlib
-
+import datetime
 from tensorflow.keras.models import Model
 from tensorflow.keras.utils import plot_model
-
+from tensorflow.keras.callbacks import ModelCheckpoint, CSVLogger, History
 from a2e.experiment import git_hash, git_diff
+from a2e.plotter import plot
 
 
 class Experiment:
 
-    def __init__(self, experiment_id: str, verbose: bool = True, out_directory: str = None):
+    def __init__(self, experiment_id: str = None, verbose: bool = True, out_directory: str = None, auto_datetime_directory: bool = True):
         self.experiment_id = experiment_id
+        self.experiment_start = datetime.datetime.now()
         self.out_directory = out_directory
         self.verbose = verbose
+
+        if self.experiment_id is None:
+            caller_frame = inspect.stack()[1]
+            caller_filename = caller_frame.filename
+
+            self.experiment_id = os.path.splitext(os.path.basename(caller_filename))[0]
 
         if self.out_directory is None:
             experiment_directory = pathlib.Path(__file__).parent.absolute()
             self.out_directory = os.path.abspath(os.path.join(experiment_directory, f'../../out/{self.experiment_id}/'))
+
+        if auto_datetime_directory:
+            self.out_directory = os.path.join(self.out_directory, self.experiment_start.strftime('%Y_%m_%d/%H_%M_%S'))
 
         if not os.path.isdir(self.out_directory):
             os.makedirs(self.out_directory)
@@ -27,23 +38,41 @@ class Experiment:
         self.log('git/diff', git_diff())
 
     def __del__(self):
-        print(f'Destructor called for Experiment with id "{self.experiment_id}"')
+        self.print(f'Destructor called for Experiment with id "{self.experiment_id}"')
 
     def log(self, key: str, value: any):
         out_file_path = self._out_file(key)
 
         with open(out_file_path, 'w') as out_file:
-            if isinstance(value, dict):
+            if isinstance(value, dict) or isinstance(value, list):
                 out_file.write(json.dumps(value, indent=2))
             else:
                 out_file.write(str(value))
+
+    def log_plot(self, key: str, x=None, y=None, xlabel=None, ylabel=None, label=None, create_figure: bool = True):
+        self.log(key, y)
+        plot(x=x, y=y, xlabel=xlabel, ylabel=ylabel, label=label, create_figure=create_figure, out_path=self._out_file(key))
+
+    def log_history(self, history: History):
+        self.log_model(history.model)
+
+        if 'loss' in history.history and 'val_loss' in history.history:
+            self.log('metrics/val_loss', history.history['val_loss'])
+            self.log_plot('metrics/loss', y=history.history['val_loss'], label='validation loss', xlabel='epoch')
+            self.log_plot('metrics/loss', y=history.history['loss'], label='loss', xlabel='epoch', create_figure=False)
+        else:
+            if 'loss' in history.history:
+                self.log_plot('metrics/loss', y=history.history['loss'], xlabel='epoch', ylabel='loss')
+
+            if 'val_loss' in history.history:
+                self.log_plot('metrics/val_loss', y=history.history['val_loss'], xlabel='epoch', ylabel='validation loss')
 
     def log_model(self, model: Model):
         model_summary = []
         model.summary(print_fn=lambda x: model_summary.append(x))
 
-        plot_model(model, show_shapes=True, expand_nested=True, to_file=self._out_file('keras/model.png'))
-        self.log('keras/summary.txt', "\n".join(model_summary))
+        plot_model(model, show_shapes=True, expand_nested=True, to_file=self._out_file('model/model.png'))
+        self.log('model/summary.txt', '\n'.join(model_summary))
 
     def _out_file(self, relative_file_path: str):
         out_file_path = os.path.join(self.out_directory, relative_file_path)
@@ -58,11 +87,21 @@ class Experiment:
         if self.verbose:
             print(f'[{self.experiment_id}] {message}')
 
+    def callbacks(self, save_best: bool = True):
+        callbacks = [
+            ModelCheckpoint(self._out_file('model/model.hdf5')),
+            CSVLogger(self._out_file('metrics/loss_log.csv'), separator=',', append=False),
+        ]
 
-def create_experiment(experiment_id: str = None) -> Experiment:
-    if experiment_id is None:
-        caller_frame = inspect.stack()[1]
-        caller_filename = caller_frame.filename
+        if save_best:
+            callbacks.append(ModelCheckpoint(
+                self._out_file('model/model.best.hdf5'),
+                save_best_only=True,
+                monitor='val_loss',
+                mode='min')
+            )
+
+        return callbacks
 
         experiment_id = os.path.splitext(os.path.basename(caller_filename))[0]
 
