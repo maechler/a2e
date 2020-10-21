e100778ec7152bfca3459a833bc4c5b1a4138c57
Markus Mächler <markus.maechler@bithost.ch>
2020-10-21 15:32:59 +0200

    Refactors plotter

--- a2e/plotter/_plotter.py
+++ a2e/plotter/_plotter.py
@@ -10,26 +10,35 @@ style_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pretty.m
 plt.style.use(style_path)
 
 
-def plot_model_layer_weights(model: Model):
+def plot_model_layer_weights(model: Model, out_path=None, show=False):
     for layer in model.layers:
         if len(layer.get_weights()) > 0:
             if isinstance(layer, Conv1D):
-                plot_conv_weights(layer.get_weights()[0], '../out/test/')
+                plot_conv_layer(layer, out_path=out_path, show=show)
             else:
-                plot_dense_weights(layer.get_weights()[0])
+                plot_dense_layer(layer, out_path=out_path, show=show)
 
 
-def plot_dense_weights(weights):
-    fig = plt.figure()
-    #fig.suptitle('layer: ' + str(layer.name))
+def plot_dense_layer(layer, out_path=None, show=False):
+    name = layer.name
+    weights = layer.get_weights()[0]
+    figure = plt.figure()
 
-    plt.matshow(weights)
+    plt.tight_layout(pad=0, w_pad=0, h_pad=0)
+    plt.matshow(weights, fignum=figure.number)
 
     ax = plt.gca()
     ax.grid(False)
 
+    figure.savefig(os.path.join(out_path, f'{name}.png'), format='png')
 
-def plot_conv_weights(weights, save_path):
+    if show:
+        plt.show()
+
+
+def plot_conv_layer(layer, out_path=None, show=False):
+    name = layer.name
+    weights = layer.get_weights()[0]
     figure = plt.figure()
     number_of_filters = weights.shape[2]
 
@@ -52,32 +61,50 @@ def plot_conv_weights(weights, save_path):
         plt.tight_layout(pad=0, w_pad=0, h_pad=0)
         plt.plot(filter)
 
-    if save_path is not None:
-        if not os.path.isdir(save_path):
-            os.makedirs(save_path)
+    figure.savefig(os.path.join(out_path, f'{name}.png'), format='png')
 
-        figure.savefig(os.path.join(save_path, 'bla.pdf'), format='pdf', dpi=400)
+    if show:
+        plt.show()
 
 
-def plot(x=None, y=None, ylabel=None, xlabel=None, label=None, out_path=None, show=False, show_legend=True, time_format=False, out_formats=['pdf', 'png'], title=None, xlim=None, ylim=None, color=None, show_screw_tightened=False, anomalous_start=None, create_figure=True):
+def plot(
+    y,
+    x=None,
+    color=None,
+    ylabel=None,
+    xlabel=None,
+    label=None,
+    title=None,
+    ylim=None,
+    xlim=None,
+    vlines=[],
+    time_formatting=False,
+    out_path=None,
+    out_formats=['png'],
+    show=False,
+    show_legend=True,
+    create_figure=True
+):
     if create_figure:
         fig, ax = plt.subplots()
     else:
         fig = plt.gcf()
         ax = plt.gca()
 
-    if time_format:
+    if time_formatting:
         ax.xaxis.set_major_locator(mdates.HourLocator())
         ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
         ax.xaxis.set_minor_locator(mdates.MinuteLocator(30))
 
     if x is None:
-        x = range(0, len(y))
+        x = range(1, len(y) + 1)
 
     ax.plot(x, y, color=color, label=label)
 
-    if show_screw_tightened:
-        ax.axvline(x=anomalous_start, color='#D4373E', linestyle='solid', label='screw_tightened')
+    for vline in vlines:
+        linestyle = vline['linestyle'] if 'linestyle' in vline else 'solid'
+
+        ax.axvline(x=vline['x'], color=vline['color'], label=vline['label'], linestyle=linestyle)
 
     if xlabel is not None:
         ax.set_xlabel(xlabel, labelpad=15)
