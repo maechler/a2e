9d7a6676e71fe85bcc197a6044f1a50e91cf6abf
Markus Mächler <markus.maechler@bithost.ch>
2020-11-02 22:49:51 +0100

    Refactors feed forward model to log predictions

--- experiments/feed_forward.py
+++ experiments/feed_forward.py
@@ -1,62 +1,68 @@
-import numpy as np
+from pandas import DataFrame
 from tensorflow.keras import regularizers
 from a2e.experiment import Experiment
 from a2e.datasets.bearing import load_data
 from a2e.models import create_feed_forward_autoencoder
 from a2e.processing.normalization import min_max_scale
+from a2e.utility import build_samples
 
 config = {
- 'input_size': 1025,
- 'encoding_size': 600,
- 'epochs': 5,
- 'fit_mode': 'per_feature',
+ 'input_size': 32,
+ 'encoding_size': 16,
+ 'epochs': 50,
  'shuffle': True,
  'validation_split': 0.1,
- 'data_column': 'fft',
+ 'fit_modes': ['per_feature', 'per_sample', 'train'],
+ 'data_columns': ['rms', 'crest', 'temperature'],
  'data_sets': ['400rpm', '800rpm', '1200rpm', 'variable_rpm'],
 }
 run_configs = {
- 'data_set': config['data_sets']
+ 'data_column': config['data_columns'],
+ 'data_set': config['data_sets'],
+ 'fit_mode': config['fit_modes'],
 }
 
-experiment = Experiment(auto_datetime_directory=False)
-experiment.log('config', config)
-experiment.log('run_configs', run_configs)
+experiment = Experiment(auto_datetime_directory=True)
+experiment.log('config/config', config)
+experiment.log('config/run_configs', run_configs)
 
 model = create_feed_forward_autoencoder(config['input_size'], config['encoding_size'], activity_regularizer=regularizers.l1())
 
 
 def run_callable(run_config: dict):
+    def pre_processing(data_frame: DataFrame):
+        samples = build_samples(data_frame.to_numpy().flatten(), config['input_size'])
+
+        if run_config['fit_mode'] == 'train':
+            return train_scaler.transform(samples)
+        else:
+            return min_max_scale(samples, fit_mode=run_config['fit_mode'])
+
+    experiment.print('Loading data')
     bearing_dataset = load_data(run_config['data_set'])
-    train_samples = bearing_dataset.train(column=config['data_column'])
-    test_samples = bearing_dataset.test(column=config['data_column'])
-    all_samples = bearing_dataset.all(column=config['data_column'])
+    data_frames = bearing_dataset.as_dict(column=run_config['data_column'])
+    train_samples = build_samples(data_frames['train'].to_numpy().flatten(), config['input_size'])
+    fit_mode = 'per_feature' if run_config['fit_mode'] == 'train' else run_config['fit_mode']
+    train_samples, train_scaler = min_max_scale(train_samples, fit_mode=fit_mode, return_scaler=True)
 
-    train_samples = min_max_scale(train_samples, fit_mode=config['fit_mode'])
-    test_samples = min_max_scale(test_samples, fit_mode=config['fit_mode'])
-    all_samples = min_max_scale(all_samples, fit_mode=config['fit_mode'])
-
+    experiment.print('Fitting model')
     history = model.fit(
         train_samples,
         train_samples,
+        verbose=0,
         epochs=config['epochs'],
         callbacks=experiment.callbacks(),
         validation_split=config['validation_split'],
         shuffle=config['shuffle'],
     )
 
-    reconstruction_all = model.predict(all_samples, verbose=1)
-    reconstruction_difference_all = reconstruction_all - all_samples
-    reconstruction_error_all = np.sum(np.abs(reconstruction_difference_all), axis=1)
-
-    reconstruction_test = model.predict(test_samples, verbose=1)
-    reconstruction_difference_test = reconstruction_test - test_samples
-    reconstruction_error_test = np.sum(np.abs(reconstruction_difference_test), axis=1)
-
     experiment.log_history(history)
     experiment.log_model(model)
-    experiment.log_plot('metrics/reconstruction_error_all', x=bearing_dataset.all(column=config['data_column'], as_numpy=False).index, y=reconstruction_error_all, time_formatting=True)
-    experiment.log_plot('metrics/reconstruction_error_test', x=bearing_dataset.test(column=config['data_column'], as_numpy=False).index, y=reconstruction_error_test, time_formatting=True)
+    experiment.log_predictions(
+        model=model,
+        data_frames=data_frames,
+        pre_processing=pre_processing,
+    )
 
 
 experiment.grid_run(run_configs, run_callable)
