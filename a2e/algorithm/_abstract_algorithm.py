ab5c731016b941889e9dc7e791fd4c1882d53746
Markus Mächler <markus.maechler@bithost.ch>
2020-11-08 10:59:34 +0100

    Removes unused algorithm module

--- a2e/algorithm/_abstract_algorithm.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC
-from a2e.pipeline import AbstractPipelineStep
-
-
-class AbstractAlgorithm(AbstractPipelineStep, ABC):
-    """Base class for output steps"""
-
-    def __init__(self, config):
-        super().__init__(config)
--- a2e/algorithm/_void.py
+++
@@ -1,8 +0,0 @@
-from a2e.algorithm import AbstractAlgorithm
-from a2e.pipeline import PipelineData
-
-
-class VoidAlgorithm(AbstractAlgorithm):
-    """Void algorithm, used as placeholder when no algorithm is configured."""
-    def process(self, pipeline_data: PipelineData):
-        pass
