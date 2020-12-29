0b1048f3d300401b3100650e52d829429c2cf4c1
Markus Mächler <markus.maechler@bithost.ch>
2020-12-29 12:25:33 +0100

    Adds plot_roc to public interface

--- a2e/plotter/__init__.py
+++ a2e/plotter/__init__.py
@@ -5,9 +5,11 @@ The :mod:`a2e.plotter` provides utility functions to plot data and models
 from ._plotter import plot
 from ._plotter import plot_model_layer_weights
 from ._plotter import plot_model_layer_activations
+from ._plotter import plot_roc
 
 __all__ = [
     'plot',
     'plot_model_layer_weights',
     'plot_model_layer_activations',
+    'plot_roc',
 ]
