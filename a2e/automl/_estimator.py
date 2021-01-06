ab07c4d712530ad3a21d199f57cd6eaa13a2da26
Markus Mächler <markus.maechler@bithost.ch>
2021-01-06 19:40:26 +0100

    Removes scikit-learn API

--- a2e/automl/_estimator.py
+++
@@ -1,6 +0,0 @@
-from tensorflow.python.keras.wrappers.scikit_learn import KerasRegressor
-
-
-class KerasEstimator(KerasRegressor):
-    def __init__(self, build_fn=None, verbose=0, **sk_params):
-        super().__init__(build_fn=build_fn, verbose=verbose, **sk_params)
