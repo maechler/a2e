8b8c85db49c7f1c722ff5f01033a91f8bed40378
Markus Mächler <markus.maechler@bithost.ch>
2021-01-07 11:34:19 +0100

    Fixes unit tests including deprecation warnings

--- tests/a2e/models/test_feed_forward.py
+++ tests/a2e/models/test_feed_forward.py
@@ -1,6 +1,6 @@
 import unittest
 from functools import partial
-from a2e.models import create_deep_feed_forward_autoencoder
+from a2e.model.keras import create_deep_feed_forward_autoencoder
 
 
 class TestUtility(unittest.TestCase):
@@ -8,20 +8,20 @@ class TestUtility(unittest.TestCase):
     def test_create_deep_feed_forward_autoencoder_shallow(self):
         model = create_deep_feed_forward_autoencoder(100, number_of_hidden_layers=1, compression_per_layer=0.5)
 
-        self.assertEquals(len(model.layers), 3)
-        self.assertEquals(model.layers[0].input_shape[0][1], 100)
-        self.assertEquals(model.layers[1].output_shape[1], 50)
-        self.assertEquals(model.layers[2].output_shape[1], 100)
+        self.assertEqual(len(model.layers), 3)
+        self.assertEqual(model.layers[0].input_shape[0][1], 100)
+        self.assertEqual(model.layers[1].output_shape[1], 50)
+        self.assertEqual(model.layers[2].output_shape[1], 100)
 
     def test_create_deep_feed_forward_autoencoder_deep(self):
         model = create_deep_feed_forward_autoencoder(120, number_of_hidden_layers=3, compression_per_layer=0.5)
 
-        self.assertEquals(len(model.layers), 5)
-        self.assertEquals(model.layers[0].input_shape[0][1], 120)
-        self.assertEquals(model.layers[1].output_shape[1], 60)
-        self.assertEquals(model.layers[2].output_shape[1], 30)
-        self.assertEquals(model.layers[3].output_shape[1], 60)
-        self.assertEquals(model.layers[4].output_shape[1], 120)
+        self.assertEqual(len(model.layers), 5)
+        self.assertEqual(model.layers[0].input_shape[0][1], 120)
+        self.assertEqual(model.layers[1].output_shape[1], 60)
+        self.assertEqual(model.layers[2].output_shape[1], 30)
+        self.assertEqual(model.layers[3].output_shape[1], 60)
+        self.assertEqual(model.layers[4].output_shape[1], 120)
 
     def test_create_deep_feed_forward_autoencoder_invalid(self):
         self.assertRaises(ValueError, partial(create_deep_feed_forward_autoencoder, 100, number_of_hidden_layers=2))
