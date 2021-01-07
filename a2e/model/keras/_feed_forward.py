8add4fdc431d35e056f92ad8c5ae4737a1b6060f
Markus Mächler <markus.maechler@bithost.ch>
2021-01-07 16:26:50 +0100

    Adds dropout and regularization to feed forward model

--- a2e/model/keras/_feed_forward.py
+++ a2e/model/keras/_feed_forward.py
@@ -1,7 +1,8 @@
 import math
 
 from tensorflow.keras import Model, Input
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.python.keras import regularizers
 from tensorflow.python.keras.optimizer_v2.adam import Adam
 from tensorflow.python.keras.optimizer_v2.gradient_descent import SGD
 
@@ -54,14 +55,30 @@ def create_deep_feed_forward_autoencoder(
     optimizer='adam',
     loss='binary_crossentropy',
     activity_regularizer=None,
+    activity_regularizer_factor=0.01,
     learning_rate=None,
     sgd_momentum=None,
+    use_dropout=False,
+    dropout_rate_input=0.2,
+    dropout_rate_encoder=0.2,
+    dropout_rate_decoder=0.2,
+    **kwargs,
 ) -> Model:
     if number_of_hidden_layers % 2 == 0:
         raise ValueError(f'Number of hidden layers must be odd, "{number_of_hidden_layers}" provided.')
 
     input_layer = Input(shape=(input_dimension,), name='input')
     layer = input_layer
+
+    if use_dropout:
+        layer = Dropout(dropout_rate_input)(layer)
+
+    if activity_regularizer == 'none':
+        activity_regularizer = None
+    elif activity_regularizer == 'l1':
+        activity_regularizer = regularizers.l1(activity_regularizer_factor)
+    elif activity_regularizer == 'l2':
+        activity_regularizer = regularizers.l2(activity_regularizer_factor)
 
     number_of_encoding_layers = int((number_of_hidden_layers - 1) / 2)
     encoding_layer_dimensions = []
@@ -71,7 +88,10 @@ def create_deep_feed_forward_autoencoder(
 
     # encoding
     for i, layer_dimension in enumerate(encoding_layer_dimensions):
-        layer = Dense(layer_dimension, activation=hidden_layer_activations, activity_regularizer=activity_regularizer, name=f'hidden_encoding_layer_{i}')(layer)
+        layer = Dense(layer_dimension, activation=hidden_layer_activations, name=f'hidden_encoding_layer_{i}')(layer)
+
+        if use_dropout:
+            layer = Dropout(dropout_rate_encoder)(layer)
 
     encoding_layer_size = int(math.pow(compression_per_layer, number_of_encoding_layers+1) * input_dimension)
     encoded = Dense(encoding_layer_size, activation=hidden_layer_activations, activity_regularizer=activity_regularizer, name='encoded')(layer)
@@ -79,7 +99,10 @@ def create_deep_feed_forward_autoencoder(
 
     # decoding
     for i, layer_dimension in enumerate(reversed(encoding_layer_dimensions)):
-        layer = Dense(layer_dimension, activation=hidden_layer_activations, activity_regularizer=activity_regularizer, name=f'hidden_decoding_layer_{i}')(layer)
+        layer = Dense(layer_dimension, activation=hidden_layer_activations, name=f'hidden_decoding_layer_{i}')(layer)
+
+        if use_dropout:
+            layer = Dropout(dropout_rate_decoder)(layer)
 
     output_layer = Dense(input_dimension, activation=output_layer_activation, name='output')(layer)
 
