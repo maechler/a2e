ed21977e3e23be98983ea1e34a7350751f7434f5
Markus Mächler <markus.maechler@bithost.ch>
2021-01-07 16:45:38 +0100

    Adds std scaler

--- a2e/processing/normalization.py
+++ a2e/processing/normalization.py
@@ -1,6 +1,6 @@
 import numpy as np
 from typing import Union, Tuple
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 
 def min_max_scale(
@@ -10,6 +10,27 @@ def min_max_scale(
     fit_mode: str = 'per_feature',
 ) -> Union[Tuple[np.ndarray, MinMaxScaler], np.ndarray]:
     scaler = MinMaxScaler(feature_range=feature_range)
+
+    if fit_mode == 'per_feature':
+        scaler.fit(data)
+        scaled_data = scaler.transform(data)
+    elif fit_mode == 'per_sample':
+        scaled_data = data
+
+        for i in range(0, len(scaled_data)):
+            scaled_data[i] = scaler.fit_transform(scaled_data[i].reshape(-1, 1)).reshape(1, -1)
+    else:
+        raise ValueError(f'Invalid fit_mode "{fit_mode}" provided.')
+
+    return (scaled_data, scaler) if return_scaler else scaled_data
+
+
+def std_scale(
+    data: np.ndarray,
+    return_scaler: bool = False,
+    fit_mode: str = 'per_feature',
+) -> Union[Tuple[np.ndarray, MinMaxScaler], np.ndarray]:
+    scaler = StandardScaler()
 
     if fit_mode == 'per_feature':
         scaler.fit(data)
