b1654e6f13514e99cbc7919539b47cf1533e7b53
Markus Mächler <markus.maechler@bithost.ch>
2021-01-10 14:52:07 +0100

    Migrates automl feed forward fft experiment to new scaler, logging and droput API

--- experiments/automl_feed_forward_fft.py
+++ experiments/automl_feed_forward_fft.py
@@ -17,11 +17,11 @@ config = {
 
 run_configs = {
     'data_set': [
-         '400rpm',
+         #'400rpm',
          '800rpm',
          '800rpm_gradual',
-         '1200rpm',
-         'variable_rpm'
+         #'1200rpm',
+         #'variable_rpm'
     ],
     'optimizer': [
         # 'BayesianGaussianProcessOptimizer',
@@ -33,19 +33,19 @@ run_configs = {
     'evaluation_function': [
         'a2e.evaluation.reconstruction_error_cost',
         'a2e.evaluation.keras.reconstruction_error_vs_compression_cost',
-        'a2e.evaluation.keras.reconstruction_error_vs_regularized_compression_cost',
-        'a2e.evaluation.keras.loss_cost',
-        'a2e.evaluation.keras.val_loss_cost',
-        'a2e.evaluation.keras.val_loss_vs_compression_cost',
-        'a2e.evaluation.health_score_cost',
-        'a2e.evaluation.min_health_score_cost',
+        #'a2e.evaluation.keras.reconstruction_error_vs_regularized_compression_cost',
+        #'a2e.evaluation.keras.loss_cost',
+        #'a2e.evaluation.keras.val_loss_cost',
+        #'a2e.evaluation.keras.val_loss_vs_compression_cost',
+        #'a2e.evaluation.health_score_cost',
+        #'a2e.evaluation.min_health_score_cost',
     ],
 }
 
 config_space = cs.ConfigurationSpace(seed=1234)
 
 config_space.add_hyperparameters([
-    csh.CategoricalHyperparameter('_pre_processing', [
+    csh.CategoricalHyperparameter('_scaler', [
         'none',
         'min_max_scale',
         'min_max_scale_per_sample',
@@ -55,19 +55,20 @@ config_space.add_hyperparameters([
 
     csh.Constant('input_dimension', value=1025),
     csh.CategoricalHyperparameter('number_of_hidden_layers', list(range(1, 10, 2))),
-    csh.UniformFloatHyperparameter('compression_per_layer', lower=0.3, upper=0.9, default_value=0.7),
+    csh.UniformFloatHyperparameter('compression_per_layer', lower=0.1, upper=0.9, default_value=0.7),
     csh.CategoricalHyperparameter('hidden_layer_activations', ['relu', 'linear', 'sigmoid', 'tanh']),
     csh.CategoricalHyperparameter('output_layer_activation', ['relu', 'linear', 'sigmoid', 'tanh']),
 
-    csh.CategoricalHyperparameter('use_dropout', [True, False]),
-    csh.UniformFloatHyperparameter('dropout_rate_input', lower=0.1, upper=0.9, default_value=0.5),
-    csh.UniformFloatHyperparameter('dropout_rate_encoder', lower=0.1, upper=0.9, default_value=0.5),
-    csh.UniformFloatHyperparameter('dropout_rate_decoder', lower=0.1, upper=0.9, default_value=0.5),
+    # csh.CategoricalHyperparameter('use_dropout', [True, False]),
+    # csh.UniformFloatHyperparameter('dropout_rate_input', lower=0.1, upper=0.9, default_value=0.5),
+    # csh.UniformFloatHyperparameter('dropout_rate_encoder', lower=0.1, upper=0.9, default_value=0.5),
+    # csh.UniformFloatHyperparameter('dropout_rate_decoder', lower=0.1, upper=0.9, default_value=0.5),
 
     csh.CategoricalHyperparameter('activity_regularizer', ['l1', 'none']),
     csh.UniformFloatHyperparameter('activity_regularizer_factor', lower=0.0001, upper=0.1, default_value=0.01),
 
     csh.UniformFloatHyperparameter('learning_rate', lower=1e-6, upper=1e-1, default_value=1e-2),
+    csh.CategoricalHyperparameter('use_learning_rate_decay', [True, False]),
     csh.CategoricalHyperparameter('loss', ['mse', 'binary_crossentropy']),
 
     csh.UniformFloatHyperparameter('sgd_momentum', lower=0.0, upper=0.99, default_value=0.9),
@@ -76,17 +77,17 @@ config_space.add_hyperparameters([
 
 # Conditions
 config_space.add_condition(cs.EqualsCondition(config_space.get_hyperparameter('sgd_momentum'), config_space.get_hyperparameter('optimizer'), 'sgd'))
-config_space.add_condition(cs.EqualsCondition(config_space.get_hyperparameter('dropout_rate_input'), config_space.get_hyperparameter('use_dropout'), True))
-config_space.add_condition(cs.EqualsCondition(config_space.get_hyperparameter('dropout_rate_encoder'), config_space.get_hyperparameter('use_dropout'), True))
-config_space.add_condition(cs.EqualsCondition(config_space.get_hyperparameter('dropout_rate_decoder'), config_space.get_hyperparameter('use_dropout'), True))
+# config_space.add_condition(cs.EqualsCondition(config_space.get_hyperparameter('dropout_rate_input'), config_space.get_hyperparameter('use_dropout'), True))
+# config_space.add_condition(cs.EqualsCondition(config_space.get_hyperparameter('dropout_rate_encoder'), config_space.get_hyperparameter('use_dropout'), True))
+# config_space.add_condition(cs.EqualsCondition(config_space.get_hyperparameter('dropout_rate_decoder'), config_space.get_hyperparameter('use_dropout'), True))
 config_space.add_condition(cs.EqualsCondition(config_space.get_hyperparameter('activity_regularizer_factor'), config_space.get_hyperparameter('activity_regularizer'), 'l1'))
 # config_space.add_condition(cs.EqualsCondition(config_space.get_hyperparameter('activity_regularizer_factor'), config_space.get_hyperparameter('activity_regularizer'), 'l2'))
 
 if __name__ == '__main__':
-    experiment = Experiment(auto_datetime_directory=True)
+    experiment = Experiment(auto_datetime_directory=False)
     experiment.log('config/config', config)
     experiment.log('config/run_configs', run_configs)
-    experiment.log('config/configuration_space', str(config_space))
+    experiment.log('config/config_space', str(config_space))
 
     def run_callable(run_config: dict):
         experiment.print('Loading data')
@@ -96,15 +97,15 @@ if __name__ == '__main__':
         experiment.print('Initializing optimizer')
         optimizer = create_optimizer(
             run_config['optimizer'],
-            configuration_space=config_space,
+            config_space=config_space,
             model=KerasModel(
                 create_model_function=create_deep_feed_forward_autoencoder,
                 evaluation_function=load_from_module(run_config['evaluation_function']),
             ),
             x=x_train,
-            max_iterations=100,
-            min_budget=10,
-            max_budget=10,
+            max_iterations=500,
+            min_budget=15,
+            max_budget=15,
             run_id=experiment.run_id,
             validation_split=config['validation_split'],
         )
@@ -122,6 +123,7 @@ if __name__ == '__main__':
         log_model_configs = [
             {'key': 'best', 'percentile_rank': 1.0},
             {'key': 'average', 'percentile_rank': 0.5},
+            {'key': 'worst_10th', 'percentile_rank': 0.1},
             {'key': 'worst', 'percentile_rank': 0.0},
         ]
 
@@ -130,7 +132,7 @@ if __name__ == '__main__':
 
             experiment.log_keras_model(keras_model.model, key=log_model_config['key'])
             experiment.log_keras_predictions(
-                keras_model.model,
+                keras_model,
                 bearing_dataset.as_dict(config['data_column']),
                 key=log_model_config['key'],
                 labels=bearing_dataset.as_dict(config['data_column'], labels_only=True),
