b2e535ccab8ae2fe3e93551b3c04de6210bc0f49
Markus Mächler <markus.maechler@bithost.ch>
2021-01-28 18:36:33 +0100

    Adds final configuration for frequency analysis benchmark

--- experiments/benchmark/frequency_analysis.py
+++ experiments/benchmark/frequency_analysis.py
@@ -7,15 +7,14 @@ from a2e.utility import z_score as compute_z_score, compute_classification_metri
 config = {
     # See https://medias.schaeffler.us/en/product/rotary/rolling-and-plain-bearings/ball-bearings/deep-groove-ball-bearings/6205-c/p/351057#Product%20Information
     'defect_frequency_orders': {
-        'BSFF': 2.3220,
-        'BPFFO': 3.5722,
-        'RPFFB': 4.6439,
-        'BPFFI': 5.4278,
+        'BPFO': 3.5722,
+        'BPFI': 5.4278,
+        'BSF': 2.3220,
     },
     'motor_diameter': 9.7,
     'bearing_diameter': 8.3,
     'frequency_bandwidth': 5,
-    #'zscore_threshold': 3,
+    # 'zscore_threshold': 3,
     'zscore_threshold_percentile': 99,
     'colors': [
         '#D4373E',
@@ -29,11 +28,12 @@ config = {
 }
 
 run_configs = [
-    {'data_set': '400rpm', 'rpm': 400},
-    {'data_set': '400rpm_gradual', 'rpm': 400},
-    {'data_set': '800rpm', 'rpm': 800},
-    {'data_set': '800rpm_gradual', 'rpm': 800},
-    {'data_set': '1200rpm', 'rpm': 1200},
+    {'rpm': 400, 'data_set': '400rpm'},
+    {'rpm': 400, 'data_set': '400rpm_v2'},
+    {'rpm': 800, 'data_set': '800rpm'},
+    {'rpm': 800, 'data_set': '800rpm_v2'},
+    {'rpm': 1200, 'data_set': '1200rpm'},
+    {'rpm': 1200, 'data_set': '1200rpm_v2'},
 ]
 
 experiment = Experiment(auto_datetime_directory=False)
@@ -56,8 +56,8 @@ def run_callable(run_config: dict):
     test_healthy, test_anomalous = bearing_dataset.test('fft', split='2fold')
 
     for i, (defect_type, defect_frequency_order) in enumerate(config['defect_frequency_orders'].items(), 0):
-        defect_frequency = int(defect_frequency_order * shaft_frequency)
-        train_means = train.iloc[:, defect_frequency - bandwidth:defect_frequency + bandwidth].median(axis=1)
+        defect_frequency = defect_frequency_order * shaft_frequency
+        train_means = train.iloc[:, max(int(defect_frequency) - bandwidth, 0):int(defect_frequency) + bandwidth].median(axis=1)
         train_means_median = train_means.median()
         train_means_mad = mad(train_means)
         train_z_scores = list(map(lambda x: compute_z_score(x, train_means_median, train_means_mad), train_means))
@@ -69,11 +69,11 @@ def run_callable(run_config: dict):
             'train_z_scores': train_z_scores,
         })
 
-        vlines.append({'x': defect_frequency, 'color': config['colors'][i], 'label': f'{defect_type}: {str(defect_frequency)}Hz', 'linestyle': 'dashed'})
+        vlines.append({'x': defect_frequency, 'color': config['colors'][i], 'label': f'{defect_type} = {"{:.2f}".format(defect_frequency)}Hz', 'linestyle': 'dashed'})
 
         if config['show_bandwidth']:
-            vlines.append({'x': defect_frequency + bandwidth, 'color': config['colors'][i], 'label': '', 'linestyle': 'dashed'})
-            vlines.append({'x': defect_frequency - bandwidth, 'color': config['colors'][i], 'label': '', 'linestyle': 'dashed'})
+            vlines.append({'x': int(defect_frequency) + bandwidth, 'color': config['colors'][i], 'label': '', 'linestyle': 'dashed'})
+            vlines.append({'x': max(int(defect_frequency) - bandwidth, 0), 'color': config['colors'][i], 'label': '', 'linestyle': 'dashed'})
 
     experiment.print('Plotting data')
     train_median = train.median()
@@ -81,9 +81,9 @@ def run_callable(run_config: dict):
     test_anomalous_median = test_anomalous.median()
     ylim = [0, max(max(test_healthy_median), max(test_anomalous_median), max(train_median)) + 0.1]
 
-    experiment.plot(y=train_median.values[:500], vlines=vlines, ylim=ylim, xlabel='frequency [Hz]',  ylabel='amplitude', title=f'{run_config["data_set"]} (train)', key='train_' + data_set_key)
-    experiment.plot(y=test_healthy_median.values[:500], vlines=vlines, ylim=ylim, xlabel='frequency [Hz]',  ylabel='amplitude', title=f'{run_config["data_set"]} (healthy)', key='healthy_' + data_set_key)
-    experiment.plot(y=test_anomalous_median.values[:500], vlines=vlines, ylim=ylim, xlabel='frequency [Hz]',  ylabel='amplitude', title=f'{run_config["data_set"]} (anomalous)', key='anomalous_' + data_set_key)
+    experiment.plot(y=train_median.values[:250], vlines=vlines, ylim=ylim, xlabel='frequency [Hz]',  ylabel='amplitude', title=f'{run_config["data_set"]} (train)', key='train_' + data_set_key)
+    experiment.plot(y=test_healthy_median.values[:250], vlines=vlines, ylim=ylim, xlabel='frequency [Hz]',  ylabel='amplitude', title=f'{run_config["data_set"]} (healthy)', key='healthy_' + data_set_key)
+    experiment.plot(y=test_anomalous_median.values[:250], vlines=vlines, ylim=ylim, xlabel='frequency [Hz]',  ylabel='amplitude', title=f'{run_config["data_set"]} (anomalous)', key='anomalous_' + data_set_key)
 
     experiment.print('Predicting test dataset')
     prediction = []
@@ -95,7 +95,7 @@ def run_callable(run_config: dict):
 
         for defect_frequency in defect_frequencies:
             zscore_threshold = config['zscore_threshold'] if 'zscore_threshold' in config else percentile(defect_frequency['train_z_scores'], config['zscore_threshold_percentile'])
-            row_mean = row.iloc[defect_frequency['frequency'] - bandwidth:defect_frequency['frequency'] + bandwidth].mean()
+            row_mean = row.iloc[max(int(defect_frequency['frequency']) - bandwidth, 0):int(defect_frequency['frequency']) + bandwidth].mean()
             z_score = compute_z_score(row_mean, defect_frequency['train_median'], defect_frequency['train_mad'])
             is_anomaly = is_anomaly or (z_score > zscore_threshold)
             largest_zscore = largest_zscore if largest_zscore > z_score else z_score
