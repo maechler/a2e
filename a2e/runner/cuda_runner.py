f9494fd04c2182a1c71cc6b7a0899b5ff14f5b11
Markus Mächler <markus.maechler@bithost.ch>
2021-02-11 17:31:22 +0100

    Changes CUDA default GPU to 0

--- a2e/runner/cuda_runner.py
+++ a2e/runner/cuda_runner.py
@@ -2,11 +2,12 @@ import os
 import argparse
 import tensorflow as tf
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('run_file', help='The path to the file to be run.', type=str)
-    parser.add_argument('--gpu', '-g', help='ID of GPU that should be used.', type=str, default="")
+    parser.add_argument('--gpu', '-g', help='ID of GPU that should be used.', type=str, default='0')
     parser.add_argument('--verbose', '-v', help='ID of GPU that should be used.', type=bool, default=False)
 
     args = parser.parse_args()
